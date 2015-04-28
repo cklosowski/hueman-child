b8bc4c5083658ec4f4710cab939681269ddb524c
Chris Klosowski <cklosowski@gmail.com>
2015-04-28 14:48:06 -0700

    Adding Twitter link to the post nav

--- single.php
+++ single.php
@@ -48,7 +48,15 @@
 		<?php if ( is_single() ): ?>
 			<h4 class="heading"><i class="fa fa-history"></i>Other posts from around this time</h4>
 			<ul class="post-nav group">
-				<li class="next"><?php next_post_link('%link', '<i class="fa fa-chevron-right"></i><strong>Next Post</strong> <span>%title</span>'); ?></li>
+				<li class="next">
+					<?php if ( ! empty( get_next_post_link() ) ) : ?>
+						<?php next_post_link('%link', '<i class="fa fa-chevron-right"></i><strong>Next Post</strong> <span>%title</span>'); ?>
+					<?php else: ?>
+						<a class="twitter-follow-button" onClick="(window.open('https://twitter.com/intent/follow?region=follow_link&screen_name=cklosowski&tw_p=followbutton','','width=800,height=600,scrollbars=no'))" href="#">
+							<i class="fa fa-twitter"></i><strong>You're all caught up!</strong> <span>Follow me @cklosowski to get the latest</span>
+						</a>
+					<?php endif; ?>
+				</li>
 				<li class="previous"><?php previous_post_link('%link', '<i class="fa fa-chevron-left"></i><strong>Previous Post</strong> <span>%title</span>'); ?></li>
 			</ul>
 		<?php endif; ?>
